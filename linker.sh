b18f878c5772a46d8edce0ed214de0fbb4ac309e
Ole Norstrand <olenorstrand@Oles-MacBook-Pro.local>
2023-11-28 09:25:58 +0100

    linker

--- linker.sh
+++ linker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Move to Home directory
 cd ~
-# Backup and remove existing .zshrc and .ideavimrc
-if [ -f .zshrc ]; then
+# Unlink existing .zshrc and .ideavimrc and backup real files
+if [ -L .zshrc ]; then
+    unlink .zshrc 
+elif [ -f .zshrc ]; then
     mv .zshrc .zshrc.bak 
 fi
-if [ -f .ideavimrc ]; then
+if [ -L .ideavimrc ]; then
+    unlink .ideavimrc
+elif [ -f .ideavimrc ]; then
     mv .ideavimrc .ideavimrc.bak
 fi
 # Create new symbolic links
